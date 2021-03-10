1f8d33ce35dffce81e6019f902033543c3f5b130
shintadewi29rpl <72966161+shintadewi29rpl@users.noreply.github.com>
2021-03-10 13:12:34 +0700

    Update child.java

--- src/belajar/child.java
+++ src/belajar/child.java
@@ -10,9 +10,14 @@ package belajar;
  * @author I d e a P a d
  */
 public class child extends parent{
+    //menurunkan class parent ke class child
+    
+    //mengisi objek
     int umur = 9;
     int jumlah = 6;
     
+    //membuat method cetak
+    //didalam method cetak, menambahkan method jenis kelamin
     public void Cetak(){
         System.out.println("Ayah Tono mempunyai hewan "+super.hewan);
         System.out.println("yang berumur  "+umur+ "tahun");
